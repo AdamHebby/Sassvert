5e763fd9a1d5a353653843c567e6d4fa5621a242
Adam Hebden <adamhebden56@googlemail.com>
2017-09-20 23:26:32 +0100

    Updated Convert to accept CLI arguments

--- Convert.php
+++ Convert.php
@@ -1,28 +1,37 @@
 <?php
 
-$html     = readline('Enter the name of the HTML file: ');
-$extraCSS = readline('Enter the name of the CSS file (if any): ');
+$htmlPrompt = "Enter the name of the HTML file: ";
+$cssPrompt  = "Enter the name of the CSS file (if any): ";
 
-if (isset($html) == false || trim($html) == '') { // Check for valid 1st input
+$html = (isset($argv[1]) && $argv[1] !== '') ? $argv[1] : readline($htmlPrompt);
+$css = (isset($argv[2]) && $argv[2] !== '') ? $argv[2] : readline($cssPrompt);
+
+if (isset($html) == false || trim($html) == '') {
+    // Check for valid 1st input
     exit("Invalid HTML File. Exiting. \n");
-} elseif (file_exists(trim($html)) == false || trim(file_get_contents(trim($html))) == '') { // Check for valid HTML file
+} elseif (
+    file_exists(trim($html)) == false || 
+    trim(file_get_contents(trim($html))) == '') {
+     // Check for valid HTML file
     exit("HTML File is Empty. Exiting. \n");
 }
 
-if (trim($extraCSS) != '' && file_exists(trim($extraCSS)) == false) { // User has defined CSS but it doesn't exist
+// User has defined CSS but it doesn't exist
+if (trim($css) !== '' && file_exists(trim($css)) == false) {
     exit("Invalid CSS File. Exiting. \n");
-} elseif (trim($extraCSS) != '' && trim(file_get_contents(trim($extraCSS))) == '') { // User has defined CSS but it's empty
+} elseif (trim($css) !== '' && trim(file_get_contents(trim($css))) == '') {
+    // User has defined CSS but it's empty
     exit("CSS File is Empty. Exiting. \n");
 }
 
-$extraCSS = trim($extraCSS);
-$html     = file_get_contents(trim($html));
+$css  = trim($css);
+$html = trim(file_get_contents(trim($html)));
 
 require_once('NodeTree.php');
 require_once('Node.php');
 
 $SassVert_Converter = new SassVert_Converter($html); // Initiate Class
 
-$SassVert_Converter->setCSSFile($extraCSS); // Include CSS file
+$SassVert_Converter->setCSSFile($css); // Include CSS file
 
 $SassVert_Converter->PrintSCSS(); // Output SCSS
